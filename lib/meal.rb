73867a46ebbab4bdd7ca406a536f819572aaa4b7
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-08 15:51:28 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,8 @@
+
+#Meal class as a 'joining' model between our Waiter and our Customer classes.
+#Will also hold the only source of truth. WIll know all the details of each Meal
+#This includes the total cost and the tip(Defaults to zero) but also the customers
+# and waiter were for each meal
 class Meal
 
 end
